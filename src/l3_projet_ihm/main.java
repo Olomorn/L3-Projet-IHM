cc4f31eb015ff171efaf95421948ca98846ee2d9
Benoit <tortimert@ymail.com>
2018-03-20 17:07:14 +0100

    Second second commit trying to fix more things

--- src/l3_projet_ihm/main.java
+++ src/l3_projet_ihm/main.java
@@ -1,9 +1,10 @@
+package l3_projet_ihm;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package l3_projet_ihm;
 
 /**
  *
