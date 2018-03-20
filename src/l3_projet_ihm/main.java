a8fb50e015968c3386692dd2994033f3c08155b2
Olomorn <32341618+Olomorn@users.noreply.github.com>
2018-03-20 17:15:07 +0100

    Update main.java

--- src/l3_projet_ihm/main.java
+++ src/l3_projet_ihm/main.java
@@ -16,7 +16,7 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //OLALALALALA C UNE aplication tottalelent ifferente mintenant !!!!
     }
     
 }
