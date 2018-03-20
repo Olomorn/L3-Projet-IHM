5f283ec0fb775d606fa3bda9ed8b24a2c85a650a
Benoit <tortimert@ymail.com>
2018-03-20 17:03:15 +0100

    Second commit trying to fix things

--- src/l3_projet_ihm/main.java
+++ src/l3_projet_ihm/main.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package l3.projet.ihm;
+package l3_projet_ihm;
 
 /**
  *
  * @author olomorn
  */
-public class L3ProjetIHM {
+public class main {
 
     /**
      * @param args the command line arguments
